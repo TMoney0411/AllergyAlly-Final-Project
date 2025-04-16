1afe4bf956f8c2446f488f0f0c5563fd4263baf3
TMoney0411 <131919508+TMoney0411@users.noreply.github.com>
2025-04-16 14:26:40 -0400

    Settings product_options.php

--- product_options.php
+++ product_options.php
@@ -16,9 +16,14 @@ $name = $_SESSION['name'];
   <meta charset="UTF-8">
   <meta name = "viewport" content = "width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
 </head>
 <body>
-  <div class="container"> <h1 id="allergy-ally-header">AllergyAlly</h1>
+  <div class="container"> 
+    <div class="settings-icon">
+      <i class="fas fa-cog" id="settingsButton"></i>
+    </div>
+    <h1 id="allergy-ally-header">AllergyAlly</h1>
     <div id="greeting-container">
       <h2 id="greeting-text">Hello <?php echo htmlspecialchars($name) . "!"; ?></h2>
     </div>
@@ -27,6 +32,12 @@ $name = $_SESSION['name'];
       <input type="button" value="Search" onclick="window.location.href='food_finder.php'"><br><br>
     </div>
     <?php include('disclaimer.php'); ?>
-  </div> <div id="location"></div>
+  </div> 
+  <div id="location"></div>
+  <div class="dropdown-menu" id="settingsDropdown">
+    <a href="edit_allergies.php">Edit Allergies</a>
+    <a href="add_allergies.php">Add Allergies</a>
+  </div>
+  <script src="settingsScript.js"></script>
 </body>
 </html>
