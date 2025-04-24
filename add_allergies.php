58fa928ed18d090af91f0f57e9f12fa5d20fa9bb
TMoney0411 <131919508+TMoney0411@users.noreply.github.com>
2025-04-24 12:28:57 -0400

    Starter add_allergies.php

---
+++ add_allergies.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang = "en">
+<head>
+    <meta charset="UTF-8">
+    <meta name = "viewport" content = "width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container" id="mainContainer">
+        <h1>Add Allergies</h1>
+        <input type="text" placeholder="Please add your new allergies in here">
+    </div>
+</body>
+</html>
